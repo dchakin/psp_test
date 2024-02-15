9895b9fded79eca509b4f42f2b3ca5037549e54c
Dmitry Chakin <64032861+dchakin@users.noreply.github.com>
2024-02-15 11:25:12 +0300

    upd

--- app.js
+++ app.js
@@ -14,11 +14,21 @@ const {
   statStr,
   checkPanStr,
   nopayStr,
+  changLengStr,
+  createCaseStr,
+  maillRequestinStr,
+  cparamStr,
+  changPmStr,
+  startStr,
+  urlStr,
+  finalStatusStr,
+  banksStr,
+  checkCardStr,
+} = require('./response.js')
 
-} = require("./response.js")
-
-const http = require("http")
-const fs = require("fs")
+const http = require('http')
+const fs = require('fs')
+const { log } = require('console')
 
 // var express = require('express')
 // var cors = require('cors')
@@ -27,17 +37,17 @@ const fs = require("fs")
 // app.use(cors())
 
 const checkUrl = (url, value) => {
-  const pathArray = url.split("/")
+  const pathArray = url.split('/')
   return pathArray[2] === value
 }
 
 const write = (res, value) => {
   res.writeHead(200, {
-    'Access-Control-Allow-Origin': "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    "Content-Type": "application/json",
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Content-Type': 'application/json',
   })
   res.end(value)
 }
@@ -46,84 +56,110 @@ const requestListener = function (req, res) {
   const time = new Date()
   const tokenDate = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
 
-  const authToken = req.headers["authorization"]
+  const authToken = req.headers['authorization']
 
-  if (checkUrl(req.url, "refresh_token")) {
+  if (checkUrl(req.url, 'refresh_token')) {
     console.log(req.url)
     write(res, token(tokenDate))
   }
-  else if (checkUrl(req.url, "requestin")) {
+  //   Waiting pages PS_ECOM
+  else if (checkUrl(req.url, 'start')) {
+    console.log(req.url)
+    write(res, startStr)
+  } else if (checkUrl(req.url, 'url')) {
+    console.log(req.url)
+    write(res, urlStr)
+  } else if (checkUrl(req.url, 'final_status')) {
+    console.log(req.url)
+    write(res, finalStatusStr)
+  } else if (checkUrl(req.url, 'banks')) {
+    console.log(req.url)
+    write(res, banksStr)
+  } else if (checkUrl(req.url, 'check_card')) {
+    console.log(req.url)
+    write(res, checkCardStr)
+  }
+  // ==========
+  else if (checkUrl(req.url, 'requestin')) {
     console.log(req.url, authToken)
     write(res, requestInStr)
-  }
-  else if (checkUrl(req.url, "withdrawal")) {
+  } else if (checkUrl(req.url, 'withdrawal')) {
     console.log(req.url, authToken)
     write(res, withdrawalStr)
-  }
-  else if (checkUrl(req.url, "check_pan")) {
+  } else if (checkUrl(req.url, 'check_pan')) {
     console.log(req.url, authToken)
     write(res, checkPanStr)
-  }
-  else if (checkUrl(req.url, "details_one")) {
+  } else if (checkUrl(req.url, 'details_one')) {
     console.log(req.url, authToken)
     write(res, detailsOneStr)
-  }
-  else if (checkUrl(req.url, "change_details_one")) {
+  } else if (checkUrl(req.url, 'change_details_one')) {
     console.log(req.url, authToken)
 
     write(res, changeDetailsOneStr)
-  } else if (checkUrl(req.url, "banklist")) {
+  } else if (checkUrl(req.url, 'banklist')) {
     console.log(req.url, authToken)
 
     write(res, bankListStr)
-  } else if (checkUrl(req.url, "saverequestin")) {
+  } else if (checkUrl(req.url, 'saverequestin')) {
     console.log(req.url, authToken)
 
     write(res, saverequestStr)
-  } else if (checkUrl(req.url, "save")) {
+  } else if (checkUrl(req.url, 'create_case')) {
+    console.log(req.url, authToken)
+
+    write(res, createCaseStr)
+  } else if (checkUrl(req.url, 'save')) {
     console.log(req.url, authToken)
 
     write(res, saveStr)
-  } else if (checkUrl(req.url, "statusrequestin")) {
+  } else if (checkUrl(req.url, 'statusrequestin')) {
     console.log(req.url, authToken, new Date().toLocaleTimeString())
 
     write(res, statusStr)
-  } else if (checkUrl(req.url, "status")) {
+  } else if (checkUrl(req.url, 'status')) {
     console.log(req.url, authToken)
 
     write(res, statStr)
-  } else if (checkUrl(req.url, "requestin_url")) {
+  } else if (checkUrl(req.url, 'requestin_url')) {
     console.log(req.url, authToken)
 
     write(res, requestinUrlStr)
-  } else if (checkUrl(req.url, "mailrequestin")) {
+  } else if (checkUrl(req.url, 'mailrequestin')) {
     console.log(req.url, authToken)
 
-    write(res, apiFormatStr({ }))
-  } else if (checkUrl(req.url, "confrequestin")) {
+    write(res, maillRequestinStr)
+  } else if (checkUrl(req.url, 'confrequestin')) {
     console.log(req.url, authToken)
 
-    write(res, apiFormatStr({ status: "created" }))
-  } else if (checkUrl(req.url, "timeextension")) {
+    write(res, apiFormatStr({ status: 'created' }))
+  } else if (checkUrl(req.url, 'timeextension')) {
     console.log(req.url, authToken)
 
-    write(res, apiFormatStr({ status: "created" }))
-  } else if (checkUrl(req.url, "resetrequestin")) {
+    write(res, apiFormatStr({ status: 'created' }))
+  } else if (checkUrl(req.url, 'resetrequestin')) {
     console.log(req.url, authToken)
 
-    write(res, "resetted")
-  } else if (checkUrl(req.url, "rejectrequestin")) {
+    write(res, 'resetted')
+  } else if (checkUrl(req.url, 'rejectrequestin')) {
     console.log(req.url, authToken)
 
     write(res, apiFormatStr())
-  } else if (checkUrl(req.url, "nopay")) {
+  } else if (checkUrl(req.url, 'nopay')) {
     console.log(req.url, authToken)
     write(res, nopayStr)
-
+  } else if (checkUrl(req.url, 'cparam')) {
+    console.log(req.url, authToken)
+    write(res, cparamStr)
+  } else if (checkUrl(req.url, 'chang_leng')) {
+    console.log(req.url, authToken)
+    write(res, changLengStr)
+  } else if (checkUrl(req.url, 'chang_pm')) {
+    console.log(req.url, authToken)
+    write(res, changPmStr)
   } else {
-    fs.readFile("./settings.html", function (err, html) {
+    fs.readFile('./settings.html', function (err, html) {
       res.writeHead(200, {
-        "Content-Type": "text/html",
+        'Content-Type': 'text/html',
       })
       res.end(html)
     })
--- response.js
+++ response.js
@@ -1,7 +1,7 @@
 const apiFormat = (value) => {
   return {
     status: 1,
-    description: "string",
+    description: 'string',
     error_cod: 0,
     data: value,
   }
@@ -12,42 +12,55 @@ const apiFormatStr = (value) => JSON.stringify(apiFormat(value))
 const token = (time) => {
   let data = {
     access_token: `token ${time}`,
-    rt: "token",
-    token_type: "string",
+    rt: 'token',
+    token_type: 'string',
   }
 
   return JSON.stringify(data)
 }
 
 const requestIn = {
-  id: 1,
-  amount: 2000.43,
+  amount_min: 500,
+  amount_max: 29345,
   amount_err: false,
   no_card: false,
-  ft: true,
-  bank_name: "bank",
-  banklist: [[1, "Сбербанк/Sberbank", "/image"],[2, "Альфа-банк/Alfa-bank", "/image"],[3, "Открытие/Otkrytie", "/image"],],
-  // banklist: [],
-  currency: 840, // RUB = 643; KZT = 398; UZS = 860; USD = 840; EUR = 978
-  rate: 1.0,
-  fee: 1.0,
-  // payment_method: { 'method': [], 'default': '' },
-  payment_method: { method: ["SBP", "P2P"], default: "P2P" },
-  status: "created",
-  // 'success_url': 'https://primefaces.org/primevue/button',
-  success_url: "",
-  // 'fail_url': 'https://primefaces.org/primevue/panel',
-  fail_url: "",
-  return_url: 'https://primefaces.org/primevue/carousel',
-  // return_url: "",
+  banklist: [[4, 'Тинькофф/Tinkoff', '/images/tinkoff.svg']],
+  status: 'created',
+  userblock: false,
+  id: 6718840,
+  paymentnum: '6718840',
   amount_edit: true,
-  amount_min: 0,
-  amount_max: 50000,
+  amount: 1200.23,
+  bank_name: 'Тинькофф',
+  currency: 643,
+  rate: 0.0,
+  fee: 0.0,
+  payment_method: {
+    method: ['P2P', 'SBP'],
+    default: 'SBP',
+  },
+  success_url: 'https://www.google.com/',
+  fail_url: 'https://www.yandex.ru/',
+  return_url: 'https://www.mozilla.org/ru/firefox/new/',
+  mail: 'support@kiberpay.com',
+  pspname: 'KiberPay',
   helpdesk: true,
-  mail: "test@test.ru",
-  // 'mail': ''
-  paymentnum: "3297434",
-  // 'psplogo': '/images/canceled-application.svg',
+  ft: true,
+  merch_name: 'Leon',
+  uid: 'ee9b2bd3-2e5a-4195-bf6c-880e252fb33c',
+  merchant_uid: 'Mylink',
+  customer_uid: '',
+  payment_method_saverequestin: 'P2P',
+  pan: '2200 7009 5932 7957',
+  phone: '9286831731',
+  qr_data: '',
+  qr_caption: '5314413561',
+  holder: 'Даудов Гасан',
+  flow: 1,
+  time_passed: 182,
+  time_passed_created: 212,
+  mail_cus: '',
+  attached: false,
 }
 const requestInStr = JSON.stringify(apiFormat(requestIn))
 
@@ -59,70 +72,105 @@ const withdrawal = {
   bank_name: null,
   bank_id: null,
   currency: 643,
-  status: "creating",
+  status: 'creating',
   mail: '',
   success_url: null,
   fail_url: null,
   return_url: null,
   banks: [
-    [1, "Сбербанк/Sberbank", "/image"],
-    [2, "Альфа-банк/Alfa-bank", "/image"],
-    [3, "Открытие/Otkrytie", "/image"],
+    [1, 'Сбербанк/Sberbank', '/image'],
+    [2, 'Альфа-банк/Alfa-bank', '/image'],
+    [3, 'Открытие/Otkrytie', '/image'],
   ],
   // payment_method: {
   //   method: ["SBP", "P2P"],
   //   default: "SBP",
   // },
   payment_method: {
-    method: ["P2P"],
-    default: "P2P",
+    method: ['SBP', 'P2P'],
+    default: 'P2P',
   },
   helpdesk: true,
 }
 const withdrawalStr = JSON.stringify(apiFormat(withdrawal))
 
 const detailsOne = {
-  amount: 500,
+  flow: 1,
+  amount: 10000,
   amount_err: false,
   amount_max: 149687,
   amount_min: 10,
-  ft: true,
-  bank: [3, "Тинькофф/Tinkoff", "/image"],
+  ft: false,
+  bank: [1, 'Тинькофф/Tinkoff', '/images/asaka.svg'],
   bank_err: false,
-  banklist: [
-    // [1, "СБЕР/SBER", "/image"],
-    // [2, "Тинькофф/Tinkoff", "/image"],
-    [3, "Тинькофф/Tinkoff", "/image"],
-  ],
+  banklist: [],
   currency: 643, // RUB = 643; KZT = 398; UZS = 860; USD = 840; EUR = 978
-  // fail_url: "https://primefaces.org/primevue/panel",
-  // return_url: 'https://primefaces.org/primevue/carousel',
-  // success_url: 'https://primefaces.org/primevue/button',
+  fail_url: 'http://localhost:8080/',
+  return_url: 'http://localhost:8080/',
+  success_url: 'https://primefaces.org/primevue/button',
   helpdesk: true,
-    // holder: "Андрей К", // sber
-  // holder: "Петр Л", // tinkoff
-  holder: "Дмитрий С", // vtb
-  mail: "test@example.com",
+  holder: 'Адхам Ильхамович Икрамов', // vtb
+  mail: 'test@example.com',
   no_card: false,
-  order: "687787",
-  // pan: "2200 7004 7261 0848", // sber
-  // pan: "5536 9138 8113 9683", // tinkoff
-  pan: "1232 1234 2432 3234", // vtb
-  payment_method: { method: ["SBP", "QR"], default: "QR" },
-  // payment_method: { method: ["P2P"], default: "P2P" },
-  // payment_method: { method: ["SBP"], default: "SBP" },
+  order: '687787',
+  pan: '1232 1234 2432 3240', // '1232 1234 2432 3240', // '0000000000000000000'
+  payment_method: { method: ['P2P', 'QR'], default: 'QR' },
+  phone: '9990709957',
+  pspname: 'KiberPay',
+  qr: 'https://www.tinkoff.ru/rm/chakin.dmitriy2/wA2Ye84222/',
+  qr_caption: '12434323424',
+  status: 'refill', // pending, timeout, reject, refill
+  userblock: false,
+  customer_uid: 'testCustomerUid',
+  merch_name: 'TestMerch',
+  merchant_uid: 'testMerchantUid',
+  uid: 'ffa66290-19f0-4681-af4e-8d65e73b12bf',
+  time_passed: 100,
 
-  phone: "9307160753",
-  pspname: "KiberPay",
-  // qr: "https://www.tinkoff.ru/rm/chakin.dmitriy2/wA2Ye84222/",
-  qr: "https://www.sberbank.com/ru/person/dl/jc?user_link=fOYfCLYuDoehaTVqL",
-  // qr: "",
-  qr_caption: "00000001",
-  // qr_bank: "sber",
-  status: "pending",
-  paymentnum: "9999999",
+  // "order": "7372311",
+  //         "status": "pending",
+  //         "amount": 777.0,
+  //         "currency": 643,
+  //         "pan": "2202 2069 4523 0311",
+  //         "phone": "+79362565740",
+  //         "qr": "",
+  //         "qr_caption": "",
+  //         "holder": "Жуков Дмитрий",
+  //         "bank": [
+  //             2,
+  //             "Сбер/Sber",
+  //             "/images/sber.svg"
+  //         ],
+  //         "banklist": [],
+  //         "no_card": false,
+  //         "amount_err": false,
+  //         "bank_err": false,
+  //         "amount_min": 10,
+  //         "amount_max": 337457,
+  //         "userblock": false,
+  //         "payment_method": {
+  //             "method": [
+  //                 "SBP"
+  //             ],
+  //             "default": "SBP"
+  //         },
+  //         "success_url": "",
+  //         "fail_url": "",
+  //         "return_url": "",
+  //         "mail": "support@kiberpay.com",
+  //         "pspname": "KiberPay",
+  //         "helpdesk": true,
+  //         "ft": true,
+  //         "flow": 1,
+  //         "merch_name": "Песочница",
+  //         "uid": "d8a410d7-efd3-4ac5-bba6-d42dc0cccb58",
+  //         "merchant_uid": "Mylink",
+  //         "customer_uid": "",
+  //         "time_passed": 515,
+  //         "time_passed_created": 518,
+  //         "mail_cus": "",
+  //         "attached": false
 
-  // order: '2038850',
   // status: 'created',
   // payment_method: {'method': [], 'default': ''},
   // amount: 35.0,
@@ -151,8 +199,8 @@ const detailsOne = {
 const detailsOneStr = JSON.stringify(apiFormat(detailsOne))
 
 const changeDetailsOne = {
-  order: "676086",
-  status: "pending",
+  order: '676086',
+  status: 'pending',
   amount: 5000.34,
   amount_err: false,
   amount_min: 10,
@@ -160,110 +208,144 @@ const changeDetailsOne = {
   currency: 643,
   // pan: "2200 7004 7261 0848", // sber
   // pan: "5536 9138 8113 9683", // tinkoff
-  pan: "1232 1234 2432 3234", // vtb
-  phone: "9231286422",
-  qr: "",
+  pan: '1232 1234 2432 3234', // vtb
+  phone: '9231286422',
+  qr: '',
   qr_caption: 207472,
   // holder: "Андрей К", // sber
   // holder: "Петр Л", // tinkoff
-  holder: "Дмитрий С", // vtb
+  holder: 'Дмитрий С', // vtb
   // bank: [1, "СБЕР/SBER", "/image"],
   // bank: [2, "Тинькофф/Tinkoff", "/image"],
-  bank: [3, "ВТБ/VTB", "/image"],
+  bank: [3, 'ВТБ/VTB', '/image'],
   bank_err: false,
   no_card: false,
   banklist: [
     // [1, "СБЕР/SBER", "/image"],
-    [2, "Тинькофф/Tinkoff", "/image"],
-    [3, "ВТБ/VTB", "/image"]
+    [2, 'Тинькофф/Tinkoff', '/image'],
+    [3, 'ВТБ/VTB', '/image'],
   ],
 }
 const changeDetailsOneStr = JSON.stringify(apiFormat(changeDetailsOne))
 
 const bankList = {
-  status: "created",
+  status: 'created',
   amount_min: 9.0,
   amount_max: 2321434.0,
   amount_err: false,
-  banklist: [[1, "Сбербанк/Sberbank", "/image"],[2, "Альфа-банк/Alfa-bank", "/image"],[4, "Тинькофф/Tinkoff", "/images/tinkoff.svg"],],
+  banklist: [
+    [1, 'Сбербанк/Sberbank', '/image'],
+    [2, 'Альфа-банк/Alfa-bank', '/image'],
+    [4, 'Тинькофф/Tinkoff', '/images/tinkoff.svg'],
+  ],
   no_card: false,
-  // banklist: [],
+  userblock: false,
+
+  // amount_min: 0,
+  // amount_max: 0,
+  // amount_err: true,
+  // bank_err: true,
   // no_card: true,
+  // banklist: [],
+  // amount: 0,
+  // payment_method: '',
+  // pan: '',
+  // phone: '',
+  // holder: '',
+  // status: '',
+  // userblock: true,
+  // flow: 1,
 }
 const bankListStr = JSON.stringify(apiFormat(bankList))
 
 const saverequest = {
+  amount_min: 500,
+  amount_max: 0,
   amount_err: false,
   no_card: false,
-  // no_card: true,
+  banklist: [[4, 'Тинькофф/Tinkoff', '/images/tinkoff.svg']],
+  //   banklist: [],
+  status: 'pending',
+  userblock: false,
+  amount: 999.0,
   bank_err: false,
-  banklist: [[1, "Сбербанк/Sberbank", "/image"],[2, "Альфа-банк/Alfa-bank", "/image"],],
-  // banklist: [],
-  payment_method: "SBP",
-  status: "created",
-  amount: 2000.43,
-  amount_min: 0,
-  amount_max: 40000.0,
-  pan: "1234 5678 1234 5678",
-  phone: "9000000000",
-  holder: "Дмитрий Ч.",
-  qr: "https://www.tinkoff.ru/rm/chakin.dmitriy2/wA2Ye84222/",
-  qr_caption: "00000001",
+  payment_method: 'P2P',
+  pan: '2200 7009 1157 5917',
+  phone: '9302204086',
+  holder: 'Зонин Кирилл',
+  flow: 1,
+  time_passed: 18,
+  time_passed_created: 212,
+  mail_cus: '',
+  attached: false,
+
+  //   amount_min: 0,
+  //   amount_max: 0,
+  //   amount_err: true,
+  //   bank_err: true,
+  //   no_card: true,
+  //   banklist: [],
+  //   amount: 0,
+  //   payment_method: '',
+  //   pan: '',
+  //   phone: '',
+  //   holder: '',
+  //   status: '',
+  //   userblock: true,
+  //   flow: 1,
 }
 const saverequestStr = JSON.stringify(apiFormat(saverequest))
 
 const save = {
   id: 23498,
   amount: 1500.34,
-  pan: "1234 5678 1234 5678",
-  phone: "",
-  bank_name: "",
-  bank_id: "4",
+  pan: '1234 5678 1234 5678',
+  phone: '',
+  bank_name: '',
+  bank_id: '4',
   currency: 643,
-  status: "created",
+  status: 'created',
   mail: null,
   success_url: null,
   fail_url: null,
   return_url: null,
-  payment_method: "SBP",
+  payment_method: 'SBP',
 }
 const saveStr = JSON.stringify(apiFormat(save))
 
 const statusrequestin = {
   amount: 10000,
-  customer_uid: "testCustomerUid",
-  // 'description': 'Ждем перевод еще/Waiting for the transfer',
-  // 'description': 'Перевод не получен/Transfer not received',
-  description: "Перевод получен/Transfer received",
-  merch_name: "TestMerch",
-  merchant_uid: "testMerchantUid",
-  uid: "ffa66290-19f0-4681-af4e-8d65e73b12bf",
-  paymentstatus: "pending",
-  paymentnum: "222",
+  description: 'Перевод получен/Transfer received/uz lang',
+  customer_uid: 'testCustomerUid',
+  merch_name: 'TestMerch',
+  merchant_uid: 'testMerchantUid',
+  uid: 'ffa66290-19f0-4681-af4e-8d65e73b12bf',
+  paymentnum: '123213',
+  paymentstatus: 'refill', // refill, timeout, pending, custconf, confirmed
 }
 const statusStr = JSON.stringify(apiFormat(statusrequestin))
 
 const status = {
-  paymentstatus: "created", // undo, done, created
-  paymentnum: "000028699",
+  paymentstatus: 'created', // undo, done, created
+  paymentnum: '000028699',
 }
 const statStr = JSON.stringify(apiFormat(status))
 
 const checkPan = {
-  error_code: "0",
-  bank: "Tinkoff",
-  bank_code1c: "",
-  bank_icon: "/images/tinkoff.svg",
-  brand: "MasterCard"
+  error_code: '0',
+  bank: 'Tinkoff',
+  bank_code1c: '',
+  bank_icon: '/images/tinkoff.svg',
+  brand: 'MasterCard',
 }
 const checkPanStr = JSON.stringify(apiFormat(checkPan))
 
 const requestinUrl = {
-  // 'success_url': 'https://primefaces.org/primevue/button',
+  //   success_url: 'https://primefaces.org/primevue/button',
   success_url: '',
-  // 'fail_url': 'https://primefaces.org/primevue/panel',
+  //   fail_url: 'https://primefaces.org/primevue/panel',
   fail_url: '',
-  // 'return_url': 'https://primefaces.org/primevue/carousel',
+  //   return_url: 'https://primefaces.org/primevue/carousel',
   return_url: '',
 }
 const requestinUrlStr = JSON.stringify(apiFormat(requestinUrl))
@@ -275,10 +357,149 @@ const rejectrequestinStr = JSON.stringify(apiFormat(rejectrequestin))
 
 const nopay = {
   status: 1,
-  description: "ok",
+  description: 'ok',
   error_cod: 0,
 }
 const nopayStr = JSON.stringify(apiFormat(nopay))
+
+const cparam = {
+  block: false,
+}
+const cparamStr = JSON.stringify(apiFormat(cparam))
+
+const createCase = {
+  status: 1,
+  description: 'ok',
+  error_cod: 0,
+}
+const createCaseStr = JSON.stringify(apiFormat(createCase))
+
+const changLeng = {
+  pan: '2200 7006 0922 2855',
+  payment_method: 'ENG',
+  phone: '9104794353',
+  status: 'pending',
+}
+const changLengStr = JSON.stringify(apiFormat(changLeng))
+
+const changPm = {
+  pan: '2200 7006 0922 2855',
+  payment_method: 'P2P',
+  phone: '9104794353',
+  status: 'pending',
+}
+const changPmStr = JSON.stringify(apiFormat(changPm))
+
+const maillRequestin = {
+  pan: '2200 7006 0922 2855',
+  payment_method: 'P2P',
+  phone: '9104794353',
+  status: 'pending',
+}
+const maillRequestinStr = JSON.stringify(apiFormat(maillRequestin))
+
+// Waiting pages PS_ECOM
+const start = {
+  order_data: '2024-01-12 08:08:15.632682+00:00',
+  order_amount: 24984.53,
+  order_id: '2234234',
+  status: 'ok',
+  // "success" – платеж оплачен
+  // "ok" – далее запрашивать url
+  // "block" – Блокировка пользователя
+  // "reject" – отмена ордера
+  // "waiting" – ответе не готов,запрашивать повторно /start
+  // "unavailable" – отмена ордера
+  fail_url: 'www.ru', // www.ru
+  success_url: 'https://success_url3.com/',
+  currency: '643',
+}
+const startStr = JSON.stringify(apiFormat(start))
+
+const url = {
+  status: 'redirect',
+  // "waiting" – ответе не готов, запрашивать повторно
+  // "redirect" – перенаправить пользователя на ссылку в поле «url»
+  // "block" – блокировка пользователя
+  // "start" – не была получена команда start с данными пользователя
+  // "reject" – отмена ордера
+  // "unavailable" – отмена ордера
+  // url: 'https://qr.nspk.ru/AD10005CL8AADKL4958BOFC2K0HINVM1?type=02\u0026bank=100000000017\u0026sum=1000\u0026cur=RUB\u0026crc=F994', // https://www.google.com/
+  url: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+    <head></head>
+    <body onload="javascript:submitForm();">
+        <form action="https://acs.processingcenter.ru/api/tds/form" method="post" hidden>
+            <input name="PaReq" type="hidden" value="2jOVz1MF/KjYxon3CysVmVHbZyDtscOXPTj1McmwkTALCk3m7sp5e95uskYbR6A5WIttBivzG5xC2GSIJJLirh9nXHiDlK4N6LixXg5rjNGEGg8FqykQn6ypacc3HVMnvhumyRuw9zFw40mZn65qJGmATHgPFwfttbNLCLDaBjNCFAUR1EDXGOe8YJKc/4i/+2C8D9M3iL/kmcpFCiENfpRlodhbsJbuBUhqHi+6EWN4xbKdakvGYvSfBGqP4qvTwG8LbuazK3xp91AYMRNBC36zgTlt7EDjuF276CGc2xGR8f18Imh+LaKWnJhJnNL3" />
+            <input name="MD" type="hidden" value="17074638631494257389" />
+            <input name="TermUrl" type="hidden" value="https://acs.processingcenter.ru/api/tds/pares?data=MmpPVnoxTUYvS2pZeG9uM0N5c1ZtVkhiWnlEdHNjT1hQVGoxTWNtd2tUQnphUVcrYlFLQ2k3bGVMWnhLZDlBaQ==" />
+        </form>
+        <script>
+        function submitForm() {
+            document.forms[0].submit();
+        }
+        </script>
+    </body>
+    </html>`,
+}
+const urlStr = JSON.stringify(apiFormat(url))
+
+const finalStatus = {
+  order_data: '2024-01-19 11:48:20.026632+00:00',
+  order_amount: 82323.14,
+  order_id: '383',
+  status: 'PENDING', // 'APPROVED' 'PENDING' 'DECLINED'
+  fail_url: 'https://fail_url3.com/',
+  success_url: 'https://success_url3.com/',
+  currency: '643',
+}
+const finalStatusStr = JSON.stringify(apiFormat(finalStatus))
+
+const banks = [
+  {
+    bankName: 'Сбербанк',
+    logoURL: 'http://pe.neopay.pro/api/logo/bank100000000111/',
+    schema: 'bank100000000111',
+    package_name: 'ru.sberbankmobile',
+  },
+  {
+    bankName: 'Тинькофф Банк',
+    logoURL: 'http://pe.neopay.pro/api/logo/bank100000000004/',
+    schema: 'bank100000000004',
+    package_name: 'com.idamob.tinkoff.android',
+  },
+  {
+    bankName: 'Банк ВТБ',
+    logoURL: 'http://pe.neopay.pro/api/logo/bank110000000005/',
+    schema: 'bank110000000005',
+    package_name: 'ru.vtb24.mobilebanking.android',
+    webClientUrl: 'https://online.vtb.ru/i/paymentSbp/',
+    isWebClientActive: 'true',
+  },
+  {
+    bankName: 'АЛЬФА-БАНК',
+    logoURL: 'http://pe.neopay.pro/api/logo/bank100000000008/',
+    schema: 'bank100000000008',
+    package_name: 'ru.alfabank.mobile.android',
+  },
+  {
+    bankName: 'Райффайзенбанк',
+    logoURL: 'http://pe.neopay.pro/api/logo/bank100000000007/',
+    schema: 'bank100000000007',
+    package_name: 'ru.raiffeisennews',
+  },
+]
+
+const checkCard = {
+  is_pan_valid: true,
+  is_cvc_valid: true,
+  is_date_valid: true,
+  errors: [],
+}
+const checkCardStr = JSON.stringify(checkCard)
+
+const banksStr = JSON.stringify(apiFormat(banks))
+// https://qr.nspk.ru/AD1000614RFIJQ2B93MO68E42OUH22HR?type=02\u0026bank=100000000017\u0026sum=1000\u0026cur=RUB\u0026crc=9F18
 
 module.exports = {
   token,
@@ -296,4 +517,14 @@ module.exports = {
   saveStr,
   statStr,
   checkPanStr,
+  changLengStr,
+  maillRequestinStr,
+  createCaseStr,
+  cparamStr,
+  changPmStr,
+  startStr,
+  urlStr,
+  finalStatusStr,
+  banksStr,
+  checkCardStr,
 }
